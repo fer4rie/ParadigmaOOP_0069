5c49980bdedbf954b2c03ba4a30588db73f2253e
fer4rie <firapuspita2106@gmail.com>
2026-04-21 15:01:02 +0700

    displaying student information with print data to mahasiswa

--- p1.cpp
+++ p1.cpp
@@ -6,3 +6,9 @@ class Mahasiswa{
         int nim;
         string nama; //perbaikan
         float nilai;
+        void printData(){ 
+            cout << "NIM = "<< nim <<endl;
+            cout << "Nama = "<< nama <<endl;
+            cout << "Nilai = "<< nilai <<endl;
+        }
+};
